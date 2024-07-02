7e184d8766d2f1cb541830d6cd97f43bd14c32d4
Layashma <108675686+Layashma@users.noreply.github.com>
2024-07-02 15:42:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+ 
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Game from './components/Game';
+import './index.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+     <Game/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
